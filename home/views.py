b12434fcd37fff4407c58b4f6a0df132be237b9b
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-03 13:48:09 +0700

    added authentication app

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
-    context = {
-
+    context = { 
+        'login_page' : reverse('user_login')
     }
 
     return render(request, 'homepage.html', context)
