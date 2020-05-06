0245607fd4ae6468deefaa8e0aeaf0fe19979828
safa <safafathya25@gmail.com>
2020-05-06 22:50:19 +0700

    change urls navigation

--- navigation/urls.py
+++ navigation/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path("admin-apotek/", views.admin_apotek_nav, name='admin_apotek_nav'),
 	path("cs/", views.cs_nav, name='cs_nav'),
-    path("konsumen/", views.konsumen_nav, name='konsumen_nav'),
-    path("kurir/", views.kurir_nav, name='kurir_nav'),
+ 	path("konsumen/", views.konsumen_nav, name='konsumen_nav'),
+ 	path("kurir/", views.kurir_nav, name='kurir_nav'),
 ]
