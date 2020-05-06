bd5c774c0768ee9f189f62eb81de611c731f0d5e
safa <safafathya25@gmail.com>
2020-05-06 22:47:18 +0700

    add navigation

--- navigation/urls.py
+++ navigation/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+	path("admin-apotek/", views.admin_apotek_nav, name='admin_apotek_nav'),
+	path("cs/", views.cs_nav, name='cs_nav'),
     path("konsumen/", views.konsumen_nav, name='konsumen_nav'),
+    path("kurir/", views.kurir_nav, name='kurir_nav'),
 ]
--- navigation/views.py
+++ navigation/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def admin_apotek_nav(request):
+	context = {
+	}
+	return render(request, 'navigation/admin_apotek.html', context)
+
+def cs_nav(request):
+	context = {
+	}
+	return render(request, 'navigation/cs.html', context)
+
 def konsumen_nav(request):
-    context = {
+	context = {
+	}
+	return render(request, 'navigation/konsumen.html', context)
 
-    }
-
-    return render(request, 'navigation/konsumen.html', context)
+def kurir_nav(request):
+	context = {
+	}
+	return render(request, 'navigation/kurir.html', context)
