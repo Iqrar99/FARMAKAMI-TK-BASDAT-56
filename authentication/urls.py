a5caf6186da9c69b20d614d44d902b0c10de5f6b
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-07 21:40:56 +0700

    fixed login map

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("konsumen/", views.user_login, name = 'user_login'),
+    path("", views.user_login, name = 'user_login'),
 ]
