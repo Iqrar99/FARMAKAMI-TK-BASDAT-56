51ef35ed0de2a4938b2419718b65f25d7f04f43b
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-21 22:57:39 +0700

    add queries in obat table

--- obat/views.py
+++ obat/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.db import connection
 from .forms import CreateObatForm, UpdateObatForm
 
 # Create your views here.
 def tabel_obat(request):
-    context = {}
+    query = """SELECT * FROM obat;"""
+
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO farmakami;")
+    cursor.execute(query)
+
+    data_obat = fetch(cursor)
+
+    context = {
+        'data_obat': data_obat
+    }
 
     return render(request, 'tabel/read_obat.html', context)
 
@@ -20,3 +31,7 @@ def update_obat(request):
     }
 
     return render(request, 'update/update_obat.html', context)
+
+def fetch(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
