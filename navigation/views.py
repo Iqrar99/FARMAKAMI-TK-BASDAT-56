f9530d1be0c64ea5f145675024126ab421320545
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-24 02:17:33 +0700

    prevent user who not logged in accessing navigate pages

--- navigation/views.py
+++ navigation/views.py
@@ -1,22 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def admin_apotek_nav(request):
-	context = {
-	}
+	context = {}
+
+	if 'nama' in request.session:
+		context['nama'] = request.session['nama']
+	else:
+		return redirect('/')
+
 	return render(request, 'navigation/admin_apotek.html', context)
 
 def cs_nav(request):
-	context = {
-	}
+	context = {}
+
+	if 'nama' in request.session:
+		context['nama'] = request.session['nama']
+	else:
+		return redirect('/')
+
 	return render(request, 'navigation/cs.html', context)
 
 def konsumen_nav(request):
-	context = {
-	}
+	context = {}
+
+	if 'nama' in request.session:
+		context['nama'] = request.session['nama']
+	else:
+		return redirect('/')
+
 	return render(request, 'navigation/konsumen.html', context)
 
 def kurir_nav(request):
-	context = {
-	}
+	context = {}
+
+	if 'nama' in request.session:
+		context['nama'] = request.session['nama']
+	else:
+		return redirect('/')
+	
 	return render(request, 'navigation/kurir.html', context)
