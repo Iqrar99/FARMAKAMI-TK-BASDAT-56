05fbe44140fe3f8aec905b320a0330330d6a786c
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-26 14:59:08 +0700

    finished create transaksi pembelian system

--- transaksi_pembelian/views.py
+++ transaksi_pembelian/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection
+from datetime import datetime
 from .forms import TransaksiPembelianForm, UpdateTransaksiPembelian
-# Create your views here.
-
 
 def tabel_transaksi_pembelian(request):
     query = """SELECT * FROM transaksi_pembelian;"""
@@ -11,7 +10,7 @@ def tabel_transaksi_pembelian(request):
     cursor.execute("SET SEARCH_PATH TO farmakami;")
     cursor.execute(query)
 
-    data_transaksi_pembelian = fetch(cursor)
+    data_transaksi_pembelian = __fetch(cursor)
     
     context = {
         'data_transaksi_pembelian' : data_transaksi_pembelian,
@@ -21,11 +20,30 @@ def tabel_transaksi_pembelian(request):
     return render(request, 'tabel/read_transaksi_pembelian.html', context)
 
 def buat_transaksi_pembelian(request):
-    context = {
-        'form': TransaksiPembelianForm(request.POST or None)
-    }
-    return render(request, 'create/create_transaksi_pembelian.html', context)
+    if 'email' not in request.session:
+        return redirect('/login/')
 
+    if request.session['role'] != 'admin-apotek':
+        return redirect(f'/navigate/{request.session["role"]}/')
+
+    form = TransaksiPembelianForm(request.POST or None)
+    context = {
+        'form': form,
+        'error': []
+    }
+
+    id_konsumen = request.POST['id_konsumen']
+
+    try:
+        __create_transaksi_beli(id_konsumen)
+        print("TRANSAKSI PEMBELIAN SUKSES DITAMBAHKAN")
+        
+        return redirect('/transaksi-pembelian/tabel/')
+
+    except:
+        print("TRANSAKSI PEMBELIAN GAGAL DITAMBAHKAN")
+
+    return render(request, 'create/create_transaksi_pembelian.html', context)
 
 def update_transaksi_pembelian(request):
     context = {
@@ -33,7 +51,39 @@ def update_transaksi_pembelian(request):
     }
     return render(request, 'update/update_transaksi_pembelian.html', context)
 
+def __create_transaksi_beli(id_konsumen):
+    """
+    function untuk menambah transaksi pembelian baru.
+    """
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO farmakami;")
+     
+    # generate id transaksi
+    cursor.execute(
+        """
+        SELECT id_transaksi_pembelian FROM transaksi_pembelian
+        ORDER BY id_transaksi_pembelian DESC
+        LIMIT 1;
+        """
+    )
+    latest_id = int(__fetch(cursor)[0]['id_transaksi_pembelian'][-3:])
+    new_id = latest_id + 1
+    if new_id < 10:
+        new_id = 'T00' + str(new_id)
+    elif new_id < 100:
+        new_id = 'T0' + str(new_id)
+    else:
+        new_id = 'T' + str(new_id)
 
-def fetch(cursor):
+    waktu_pembelian = str(datetime.now())
+
+    cursor.execute(
+        f"""
+        INSERT INTO transaksi_pembelian
+        VALUES ('{new_id}', '{waktu_pembelian}', 0, '{id_konsumen}');
+        """
+    )
+
+def __fetch(cursor):
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
