1727ca4ad149f6db7c9c248e0728b6ae99bdd978
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-28 17:36:52 +0700

    disabled CACHE location

--- farmakami/settings.py
+++ farmakami/settings.py
@@ -150,9 +150,9 @@ STATICFILES_DIRS = [
 
 # Cache setup
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': '192.168.221.101:11211',
+#     }
+# }
