ef9a1bead94fc1582b020f750e42381f35a784ef
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-05-28 11:51:32 +0700

    [DEBUG] switched SESSION_ENGINE to cache_db

--- farmakami/settings.py
+++ farmakami/settings.py
@@ -148,4 +148,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_ENGINE = "django.contrib.sessions.backends.cache_db"
